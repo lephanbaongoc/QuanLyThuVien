021a2f1a91f1f7b29f3454a59eda9984a53b7b1c
NguyenBaoUyen <145897661+NguyenBaoUyen@users.noreply.github.com>
2024-11-21 22:57:24 +0700

    Create footer.php

---
+++ admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>All rights reserved</b>
+    </div>
+  <strong>Copyright &copy; 2024 <a href="https://github.com/lephanbaongoc/QuanLyThuVien">QLTV php</a></strong>
+</footer>
