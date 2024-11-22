d690a9a30f3f38624f63a2207a9bb275cda78c6f
lephanbaongoc <lpbn2003@gmail.com>
2024-11-22 14:55:45 +0700

    add category's buttons

--- admin/category.php
+++ admin/category.php
@@ -86,18 +86,32 @@
 <?php include 'includes/scripts.php'; ?>
 <script>
 $(function(){
-  $(document).on('click', '.edit', function(e){
-    
+    $(document).on('click', '.edit', function(e){
+    e.preventDefault();
+    $('#edit').modal('show');
+    var id = $(this).data('id');
+    getRow(id);
   });
 
   $(document).on('click', '.delete', function(e){
-    
+    e.preventDefault();
+    $('#delete').modal('show');
+    var id = $(this).data('id');
+    getRow(id);
   });
 });
 
 function getRow(id){
   $.ajax({
-    
+    type: 'POST',
+    url: 'category_row.php',
+    data: {id:id},
+    dataType: 'json',
+    success: function(response){
+      $('.catid').val(response.id);
+      $('#edit_name').val(response.name);
+      $('#del_cat').html(response.name);
+    }
   });
 }
 </script>
