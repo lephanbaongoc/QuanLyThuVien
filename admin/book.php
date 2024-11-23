a45bec9a286c96da5047c70556c97ee56f474180
lephanbaongoc <lpbn2003@gmail.com>
2024-11-23 11:44:03 +0700

    Update book.php

---
+++ admin/book.php
@@ -0,0 +1,133 @@
+<?php include 'includes/session.php'; ?>
+<?php
+  $catid = 0;
+  $where = '';
+  if(isset($_GET['category'])){
+    $catid = $_GET['category'];
+    $where = 'WHERE books.category_id = '.$catid;
+  }
+
+?>
+<?php include 'includes/header.php'; ?>
+<body class="hold-transition skin-blue sidebar-mini">
+<div class="wrapper">
+
+  <?php include 'includes/navbar.php'; ?>
+  <?php include 'includes/menubar.php'; ?>
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        Book List
+      </h1>
+      <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li>Books</li>
+        <li class="active">Book List</li>
+      </ol>
+    </section>
+    <!-- Main content -->
+    <section class="content">
+      <?php
+       
+      ?>
+      <div class="row">
+        <div class="col-xs-12">
+          <div class="box">
+            <div class="box-header with-border">
+              <a href="#addnew" data-toggle="modal" class="btn btn-primary btn-sm btn-flat"><i class="fa fa-plus"></i> New</a>
+              <div class="box-tools pull-right">
+                <form class="form-inline">
+                  <div class="form-group">
+                    <label>Category: </label>
+                    <select class="form-control input-sm" id="select_category">
+                      <option value="0">ALL</option>
+                      <?php
+                        $sql = "SELECT * FROM category";
+                        $query = $conn->query($sql);
+                        while($catrow = $query->fetch_assoc()){
+                          $selected = ($catid == $catrow['id']) ? " selected" : "";
+                          echo "
+                            <option value='".$catrow['id']."' ".$selected.">".$catrow['name']."</option>
+                          ";
+                        }
+                      ?>
+                    </select>
+                  </div>
+                </form>
+              </div>
+            </div>
+            <div class="box-body">
+              <table id="example1" class="table table-bordered">
+                <thead>
+                  <th>Category</th>
+                  <th>ISBN</th>
+                  <th>Title</th>
+                  <th>Author</th>
+                  <th>Publisher</th>
+                  <th>Status</th>
+                  <th>Tools</th>
+                </thead>
+                <tbody>
+                  <?php
+                    $sql = "SELECT *, books.id AS bookid FROM books LEFT JOIN category ON category.id=books.category_id $where";
+                    $query = $conn->query($sql);
+                    while($row = $query->fetch_assoc()){
+                      if($row['status']){
+                        $status = '<span class="label label-danger">borrowed</span>';
+                      }
+                      else{
+                        $status = '<span class="label label-success">available</span>';
+                      }
+                      echo "
+                        <tr>
+                          <td>".$row['name']."</td>
+                          <td>".$row['isbn']."</td>
+                          <td>".$row['title']."</td>
+                          <td>".$row['author']."</td>
+                          <td>".$row['publisher']."</td>
+                          <td>".$status."</td>
+                          <td>
+                            <button class='btn btn-success btn-sm edit btn-flat' data-id='".$row['bookid']."'><i class='fa fa-edit'></i> Edit</button>
+                            <button class='btn btn-danger btn-sm delete btn-flat' data-id='".$row['bookid']."'><i class='fa fa-trash'></i> Delete</button>
+                          </td>
+                        </tr>
+                      ";
+                    }
+                  ?>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>   
+  </div>
+    
+  <?php include 'includes/footer.php'; ?>
+  <?php include 'includes/book_modal.php'; ?>
+</div>
+<?php include 'includes/scripts.php'; ?>
+<script>
+$(function(){
+  $('#select_category').change(function(){
+    
+  });
+
+  $(document).on('click', '.edit', function(e){
+    
+  });
+
+  $(document).on('click', '.delete', function(e){
+    
+  });
+});
+
+function getRow(id){
+  
+}
+</script>
+</body>
+</html>
