7122a5e05287d98490ac7017d43b318c884e646d
NguyenBaoUyen <145897661+NguyenBaoUyen@users.noreply.github.com>
2024-11-24 17:38:55 +0700

    Update category_modal.php

--- admin/includes/category_modal.php
+++ admin/includes/category_modal.php
@@ -36,35 +36,47 @@
     </div>
 </div>
 
-
-<!-- Edit -->
-<div class="modal fade" id="edit">
+<!-- Edit Category Modal -->
+<div class="modal fade" id="edit" tabindex="-1" role="dialog" aria-labelledby="editCategoryLabel" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
+            <!-- Modal Header -->
             <div class="modal-header">
-              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                  <span aria-hidden="true">&times;</span></button>
-              <h4 class="modal-title"><b>Edit Category</b></h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+                <h4 class="modal-title" id="editCategoryLabel"><b>Edit Category</b></h4>
             </div>
-            <div class="modal-body">
-              <form class="form-horizontal" method="POST" action="category_edit.php">
-                <input type="hidden" class="catid" name="id">
-                <div class="form-group">
-                    <label for="edit_name" class="col-sm-3 control-label">Name</label>
 
-                    <div class="col-sm-9">
-                      <input type="text" class="form-control" id="edit_name" name="name">
+            <!-- Modal Body -->
+            <div class="modal-body">
+                <form class="form-horizontal" method="POST" action="category_edit.php">
+                    <!-- Hidden Category ID -->
+                    <input type="hidden" class="catid" name="id">
+
+                    <!-- Name Field -->
+                    <div class="form-group">
+                        <label for="edit_name" class="col-sm-3 control-label">Name</label>
+                        <div class="col-sm-9">
+                            <input type="text" class="form-control" id="edit_name" name="name" placeholder="Enter category name" required>
+                        </div>
                     </div>
-                </div>
+                </form>
             </div>
+
+            <!-- Modal Footer -->
             <div class="modal-footer">
-              <button type="button" class="btn btn-default btn-flat pull-left" data-dismiss="modal"><i class="fa fa-close"></i> Close</button>
-              <button type="submit" class="btn btn-success btn-flat" name="edit"><i class="fa fa-check-square-o"></i> Update</button>
-              </form>
+                <button type="button" class="btn btn-default btn-flat pull-left" data-dismiss="modal">
+                    <i class="fa fa-close"></i> Close
+                </button>
+                <button type="submit" class="btn btn-success btn-flat" name="edit">
+                    <i class="fa fa-check-square-o"></i> Update
+                </button>
             </div>
         </div>
     </div>
 </div>
+
 
 <!-- Delete -->
 <div class="modal fade" id="delete" tabindex="-1" role="dialog" aria-labelledby="deleteModalLabel" aria-hidden="true">
