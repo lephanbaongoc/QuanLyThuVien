066906820b6b0f361b08f554e86724ff47d8c161
NguyenBaoUyen <145897661+NguyenBaoUyen@users.noreply.github.com>
2024-11-24 10:51:47 +0700

    Update borrow_modal.php

--- admin/includes/borrow_modal.php
+++ admin/includes/borrow_modal.php
@@ -7,29 +7,36 @@
               		<span aria-hidden="true">&times;</span></button>
             	<h4 class="modal-title"><b>Borrow Books</b></h4>
           	</div>
-          	<div class="modal-body">
-            	<form class="form-horizontal" method="POST" action="borrow_add.php">
-          		  <div class="form-group">
-                  	<label for="student" class="col-sm-3 control-label">Student ID</label>
-
-                  	<div class="col-sm-9">
-                    	<input type="text" class="form-control" id="student" name="student" required>
-                  	</div>
-                </div>
-                <div class="form-group">
-                    <label for="isbn" class="col-sm-3 control-label">ISBN</label>
-
-                    <div class="col-sm-9">
-                      <input type="text" class="form-control" id="isbn" name="isbn[]" required>
+          	<!-- Modal Body -->
+			  <div class="modal-body">
+                <form class="form-horizontal" method="POST" action="borrow_add.php">
+                    <!-- Student ID -->
+                    <div class="form-group">
+                        <label for="student" class="col-sm-3 control-label">Student ID</label>
+                        <div class="col-sm-9">
+                            <input type="text" class="form-control" id="student" name="student" placeholder="Enter Student ID" required>
+                        </div>
                     </div>
-                </div>
-                <span id="append-div"></span>
-                <div class="form-group">
-                    <div class="col-sm-9 col-sm-offset-3">
-                      <button class="btn btn-primary btn-xs btn-flat" id="append"><i class="fa fa-plus"></i> Book Field</button>
+
+                    <!-- ISBN -->
+                    <div class="form-group">
+                        <label for="isbn" class="col-sm-3 control-label">ISBN</label>
+                        <div class="col-sm-9">
+                            <input type="text" class="form-control" id="isbn" name="isbn[]" placeholder="Enter ISBN" required>
+                        </div>
                     </div>
-                </div>
-          	</div>
+
+                    <!-- Dynamic Book Fields -->
+                    <span id="append-div"></span>
+                    <div class="form-group">
+                        <div class="col-sm-9 col-sm-offset-3">
+                            <button type="button" class="btn btn-primary btn-xs btn-flat" id="append">
+                                <i class="fa fa-plus"></i> Add Book Field
+                            </button>
+                        </div>
+                    </div>
+                </form>
+            </div>
           	<div class="modal-footer">
             	<button type="button" class="btn btn-default btn-flat pull-left" data-dismiss="modal"><i class="fa fa-close"></i> Close</button>
             	<button type="submit" class="btn btn-primary btn-flat" name="add"><i class="fa fa-save"></i> Save</button>
