452b5aa6cbe48a04305cd9a2850febe0c42cc80d
NguyenBaoUyen <145897661+NguyenBaoUyen@users.noreply.github.com>
2024-11-25 10:28:41 +0700

    Update return_modal.php

--- admin/includes/return_modal.php
+++ admin/includes/return_modal.php
@@ -7,35 +7,29 @@
               		<span aria-hidden="true">&times;</span></button>
             	<h4 class="modal-title"><b>Return Books</b></h4>
           	</div>
-          	<!-- Modal Body -->
-			  <div class="modal-body">
-                <form class="form-horizontal" method="POST" action="return_add.php">
-                    <!-- Student ID -->
-                    <div class="form-group">
-                        <label for="student" class="col-sm-3 control-label">Student ID</label>
-                        <div class="col-sm-9">
-                            <input type="text" class="form-control" id="student" name="student" placeholder="Enter student ID" required>
-                        </div>
-                    </div>
+          	<div class="modal-body">
+            	<form class="form-horizontal" method="POST" action="return_add.php">
+          		  <div class="form-group">
+                  	<label for="student" class="col-sm-3 control-label">Student ID</label>
 
-                    <!-- ISBN -->
-                    <div class="form-group">
-                        <label for="isbn" class="col-sm-3 control-label">ISBN</label>
-                        <div class="col-sm-9">
-                            <input type="text" class="form-control" id="isbn" name="isbn[]" placeholder="Enter ISBN" required>
-                        </div>
+                  	<div class="col-sm-9">
+                    	<input type="text" class="form-control" id="student" name="student" required>
+                  	</div>
+                </div>
+                <div class="form-group">
+                    <label for="isbn" class="col-sm-3 control-label">ISBN</label>
+
+                    <div class="col-sm-9">
+                      <input type="text" class="form-control" id="isbn" name="isbn[]" required>
                     </div>
-                    <!-- Dynamic ISBN Field -->
-                    <div id="append-div"></div>
-                    <div class="form-group">
-                        <div class="col-sm-9 col-sm-offset-3">
-                            <button type="button" class="btn btn-primary btn-xs btn-flat" id="append">
-                                <i class="fa fa-plus"></i> Add Another Book
-                            </button>
-                        </div>
+                </div>
+                <span id="append-div"></span>
+                <div class="form-group">
+                    <div class="col-sm-9 col-sm-offset-3">
+                      <button class="btn btn-primary btn-xs btn-flat" id="append"><i class="fa fa-plus"></i> Book Field</button>
                     </div>
-                </form>
-            </div>
+                </div>
+          	</div>
           	<div class="modal-footer">
             	<button type="button" class="btn btn-default btn-flat pull-left" data-dismiss="modal"><i class="fa fa-close"></i> Close</button>
             	<button type="submit" class="btn btn-primary btn-flat" name="add"><i class="fa fa-save"></i> Save</button>
